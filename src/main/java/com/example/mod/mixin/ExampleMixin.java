751f87b80f0f3a1e69b711c7359b3ced0d2b5d0c
MushroomMif <mushroommif@yandex.ru>
2024-10-26 22:42:29 +0300

    Add entrypoint and bump game version

--- src/main/java/com/example/mod/mixin/ExampleMixin.java
+++ src/main/java/com/example/mod/mixin/ExampleMixin.java
@@ -1,15 +1,16 @@
 package com.example.mod.mixin;
 
-import aoc.kingdoms.lukasz.jakowski.AA_Game;
+import aoc.kingdoms.lukasz.menu.MenuManager;
+import aoc.kingdoms.lukasz.menu.View;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AA_Game.class)
+@Mixin(MenuManager.class)
 public class ExampleMixin {
-    @Inject(method = "create", at = @At("HEAD"))
-    private void exampleInject(CallbackInfo ci) {
-        System.out.println("Hello Fabric world!");
+    @Inject(method = "getViewID(Laoc/kingdoms/lukasz/menu/View;)I", at = @At("HEAD"))
+    private void exampleInject(View eView, CallbackInfoReturnable<Integer> cir) {
+        System.out.println(eView + " menu was opened");
     }
 }
